7ac6e06716ea13daafd191d25daac5c4f49f95ef
Thaysa Borges <borges.thaysa@gmail.com>
2024-03-21 11:12:51 -0300

    Realiza testes unitários

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,8 +1,19 @@
 import App from './App.js';
+import { exibeCard } from './App.js';
+
 
 describe('App', () => {
   it('should render without crashing', () => {
     const el = App();
     expect(el instanceof HTMLElement).toBe(true);
   });
+
+  it('espera-se que a função exibeCard retorne o HTML da carta', () => {
+    const card = exibeCard('url');
+    expect(card instanceof HTMLDivElement).toBe(true);
+  })
 });
+
+
+
+
--- src/data/mocks/datamock.js
+++ src/data/mocks/datamock.js
@@ -1,8 +1,8 @@
 export default {
     name: 'letras',
     items: [
-      { id: 'bulbasaur', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#339933' },
-      { id: 'ivysaur', image: 'https://www.serebii.net/pokemongo/pokemon/002.png', bgColor: '#339933' },
-      { id: 'venusaur', image: 'https://www.serebii.net/pokemongo/pokemon/003.png', bgColor: '#339933' },
+      { id: 'cardA', image: "../assets/A.png" },
+      { id: 'cardE', image: "../assets/E.png" },
+      { id: 'cardI', image: "../assets/I.png" },
     ],
   };
